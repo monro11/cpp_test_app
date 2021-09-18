ef1053f9acbc439f692ffde68fe3f6b5c58fbfc1
Vitalii Yevtushenko <vitalii.yevtushenko@gmail.com>
2021-09-19 02:47:04 +0300

    changed bash script

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ if ! [ -f `which docker 2>&1|awk '{print $1$2}'` ]; then  curl -sSL https://get.
 echo "FROM gcc:latest
 ADD ./ /app/
 WORKDIR /app/
-ARG GREETING=\"Hi there\"
-ENV GREETING $GREETING
-RUN sed -i 's#Greeting.*#Greeting = '"$GREETING"'#g' settings.conf && g++ ./main.cpp && ./a.out
-CMD sed -i 's#Greeting.*#Greeting = '"$GREETING"'#g' settings.conf && g++ ./main.cpp && ./a.out"  > Dockerfile
+ENV GREETING=\"Hi there\"
+RUN g++ ./main.cpp && ./a.out
+CMD sed -i 's#Greeting.*#Greeting = '\"\$GREETING\"'#g' settings.conf && g++ ./main.cpp && ./a.out"  > Dockerfile
 docker build -t intellias-cpp-test ./
 docker tag  intellias-cpp-test:latest monro11/intellias-cpp-test:latest
 docker push monro11/intellias-cpp-test:latest
