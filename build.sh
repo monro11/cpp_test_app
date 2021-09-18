54a40194318f58a73b0cedf411533e91780cacf8
Vitalii Yevtushenko <vitalii.yevtushenko@gmail.com>
2021-09-19 02:21:19 +0300

    changed bash srcipt

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+git clone https://github.com/monro11/cpp_test_app.git 
+cd ./cpp_test_app
 if ! [ -f `which docker 2>&1|awk '{print $1$2}'` ]; then  curl -sSL https://get.docker.com/ | sh  ; service docker start ; fi
 echo "FROM gcc:latest
 ADD ./ /app/
 WORKDIR /app/
-ARG GREETING="Hi there"
+ARG GREETING=\"Hi there\"
 ENV GREETING $GREETING
 RUN sed -i 's#Greeting.*#Greeting = '"$GREETING"'#g' settings.conf && g++ ./main.cpp && ./a.out
 CMD sed -i 's#Greeting.*#Greeting = '"$GREETING"'#g' settings.conf && g++ ./main.cpp && ./a.out"  > Dockerfile
-docker build -t docker-intellias-cpp ./
-docker tag  docker-intellias-cpp-test:latest monro11/intellias-cpp-test:latest
+docker build -t intellias-cpp-test ./
+docker tag  intellias-cpp-test:latest monro11/intellias-cpp-test:latest
 docker push monro11/intellias-cpp-test:latest
