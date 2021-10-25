45af177cf3162d6547944a8f2a0a1ca9e771282d
Vitalii Yevtushenko <monro_11@VITYEVTUSHENKO-MBP.local>
2021-10-25 14:07:13 +0300

    some change

--- build.sh
+++ build.sh
@@ -11,4 +11,3 @@ CMD sed -i 's#Greeting.*#Greeting = '\"\$GREETING\"'#g' settings.conf && g++ ./m
 docker build -t intellias-cpp-test ./
 docker tag  intellias-cpp-test:latest monro11/intellias-cpp-test:latest
 docker push monro11/intellias-cpp-test:latest
-#some comment
